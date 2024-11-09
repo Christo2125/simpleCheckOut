09c6f9ce74e243dd940cc00f9b345cf4b3c66828
kiran christo clement f <111306060+Christo2125@users.noreply.github.com>
2024-11-10 01:13:20 +0530

    assignment.js

--- assignment.js
+++ assignment.js
@@ -1,12 +1,9 @@
-// Toggle expansion of the pricing boxes
+
 document.querySelectorAll('.pricing-box').forEach((box) => {
     box.addEventListener('click', function () {
-        // Remove 'expanded' class from other boxes
         document.querySelectorAll('.pricing-box').forEach((b) => {
             b.classList.remove('expanded');
         });
-
-        // Add 'expanded' class to the clicked box
         this.classList.add('expanded');
     });
 });
